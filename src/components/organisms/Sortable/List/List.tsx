742f47e094823145b94db8f3c1332ed5d43336d2
ThejanNim <thejannims@gmail.com>
2024-05-16 14:38:15 +0530

    Refactor List component.

--- src/components/organisms/Sortable/List/List.tsx
+++ src/components/organisms/Sortable/List/List.tsx
@@ -1,33 +1,13 @@
-import React, { forwardRef } from 'react';
-// import classNames from 'classnames';
-
-import styles from './List.module.css';
-import clsx from 'clsx';
-
 export interface Props {
     children: React.ReactNode;
-    columns?: number;
-    style?: React.CSSProperties;
-    horizontal?: boolean;
 }
 
-export const List = forwardRef<HTMLUListElement, Props>(
-    ({ children, columns = 1, horizontal, style }: Props, ref) => {
-        return (
-            <ul
-                ref={ref}
-                style={
-                    {
-                        ...style,
-                        '--columns': columns,
-                    } as React.CSSProperties
-                }
-                className={clsx(styles.List, horizontal && styles.horizontal)}
-            >
-                {children}
-            </ul>
-        );
-    }
-);
-
-List.displayName = 'List';
+export const List = (props: Props) => {
+    return (
+        <ul
+            className="grid auto-rows-max box-border min-w-[350px] gap-[10px] px-5 pt-5 m-[10px] rounded-md min-h-[200px] transition-colors grid-cols-1"
+        >
+            {props.children}
+        </ul>
+    );
+};
