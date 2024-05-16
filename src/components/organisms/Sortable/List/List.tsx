31069ccb59d6c7161941ea93773d9e792bc89d6b
ThejanNim <thejannims@gmail.com>
2024-05-16 15:45:05 +0530

    Refactor List component.

--- src/components/organisms/Sortable/List/List.tsx
+++ src/components/organisms/Sortable/List/List.tsx
@@ -1,12 +1,12 @@
+import styles from './List.module.scss';
+
 export interface Props {
     children: React.ReactNode;
 }
 
 export const List = (props: Props) => {
     return (
-        <ul
-            className="grid auto-rows-max box-border min-w-[350px] gap-[10px] px-5 pt-5 m-[10px] rounded-md min-h-[200px] transition-colors grid-cols-1"
-        >
+        <ul className={styles.List} >
             {props.children}
         </ul>
     );
