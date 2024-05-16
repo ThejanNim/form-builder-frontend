355176b1ab46ab7347dffd3f8164ebe8f58a2ea8
ThejanNim <thejannims@gmail.com>
2024-05-16 15:39:15 +0530

    Refactor Action component.

--- src/components/organisms/Sortable/Item/components/Action/Action.tsx
+++ src/components/organisms/Sortable/Item/components/Action/Action.tsx
@@ -1,24 +1,22 @@
+import styles from './Action.module.scss';
+
 export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
   onMouseDown: (event: React.MouseEvent<HTMLButtonElement>) => void;
   onTouchStart: (event: React.TouchEvent<HTMLButtonElement>) => void;
   onKeyDown: (event: React.KeyboardEvent<HTMLButtonElement>) => void;
   children?: React.ReactNode;
-  handleProps?: any;
-  listeners?: any;
 }
 
 export const Action = (props: Props) => {
-  const { children } = props;
-
   return (
     <button
-      className="flex w-3 p-4 items-center justify-center touch-none border-none rounded outline-none appearance-none bg-transparent cursor-grab"
+      className={styles.Action}
       tabIndex={0}
       onMouseDown={props.onMouseDown}
       onTouchStart={props.onTouchStart}
       onKeyDown={props.onKeyDown}
     >
-      {children}
+      {props.children}
     </button>
   );
 };
