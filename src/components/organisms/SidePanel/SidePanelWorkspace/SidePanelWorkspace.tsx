e7fefa29276b9db09485c0d4cefc193d32fd50b4
ThejanNim <thejannims@gmail.com>
2024-08-05 15:03:33 +0530

    feat(component): add SidePanelWorkspace component

---
+++ src/components/organisms/SidePanel/SidePanelWorkspace/SidePanelWorkspace.tsx
@@ -0,0 +1,9 @@
+import { SidePanelGroupLabel } from "../SidePanelGroupLabel";
+
+export function SidePanelWorkspace() {
+  return (
+    <div>
+        <SidePanelGroupLabel label="Workspace" />
+    </div>
+  );
+}
---
+++ src/components/organisms/SidePanel/SidePanelWorkspace/index.tsx
@@ -0,0 +1,1 @@
+export { SidePanelWorkspace } from "./SidePanelWorkspace";
